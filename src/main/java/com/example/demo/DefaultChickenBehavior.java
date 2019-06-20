d9b9c791ecd4e71eab953b251a3e26fdc0554118
Lewis <ayccc@qq.com>
2019-06-20 18:31:07 +0800

    chicken runner

--- src/main/java/com/example/demo/DefaultChickenBehavior.java
+++ src/main/java/com/example/demo/DefaultChickenBehavior.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,6 +14,11 @@ import java.util.concurrent.TimeUnit;
 public class DefaultChickenBehavior implements ChickenBehavior {
     Chicken chicken;
 
+    static final int MAX_ENERGY = 10;  //小鸡 持有最大能量
+    static final int MIN_ENERGY = 5;   //小鸡持有最小能量
+    static final int ENERGY_TO_EGG_THRESHOLD = 3;  //能量转化为鸡蛋的阈值
+    static final int HEALTH_INDEX_THRESHOLD = 50;//小鸡健康指数的阈值  小于阈值 小鸡将不能正常生产
+
     DefaultChickenBehavior(Chicken chicken) {
         this.chicken = chicken;
     }
@@ -17,18 +26,49 @@ public class DefaultChickenBehavior implements ChickenBehavior {
     @Override
     public void eat(int energy) {
         Assert.notNull(chicken, "小鸡未确定");
-        this.chicken.energy += energy;
-        //System.out.println(this.chicken);
+        synchronized (this.chicken) {
+            if (this.chicken.energy >= MAX_ENERGY) {
+                try {
+                    this.chicken.wait();
+                    System.out.println("continue");
+                    this.chicken.energy += energy;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("eating....." + this.chicken.getEnergy());
+                this.chicken.energy += energy;
+            }
+            this.chicken.notify();
+        }
     }
 
     @Override
-    public int layEggs(int needEggCount) {
+    public void layEggs() {
         Assert.notNull(chicken, "小鸡未确定");
-        return needEggCount;
+        synchronized (this.chicken) {
+            //这里保证小鸡产蛋后能量不能小于最小能量
+            if (this.chicken.energy <= (MIN_ENERGY + ENERGY_TO_EGG_THRESHOLD)) {
+                try {
+                    this.chicken.wait();
+                    System.out.println("ceshi lay");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                this.chicken.energy -= ENERGY_TO_EGG_THRESHOLD;
+                this.chicken.eggNum++;
+            }
+            this.chicken.notify();
+        }
     }
 
     @Override
     public Chicken getChicken() {
         return this.chicken;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
