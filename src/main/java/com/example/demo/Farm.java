da84acf48323255ab9c9c147f799dc8e90750ecf
Lewis <ayccc@qq.com>
2019-06-20 14:30:00 +0800

    init

--- src/main/java/com/example/demo/Farm.java
+++ src/main/java/com/example/demo/Farm.java
@@ -92,7 +92,7 @@ public class Farm {
         this.feedChickens();
         this.showChickenTotalEnergy();
         System.out.println(this.getTotalEnergy());
-        this.showChickens();  /// rtr
+        this.showChickens();  /// rtr ytyty
 
     }
 
