20bad357695edc26284d9765ebbeb569a08a8efc
Lewis <ayccc@qq.com>
2019-06-24 18:36:11 +0800

    chicken runner

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Vector;
 
 @RestController
@@ -22,4 +24,15 @@ public class TestController {
             System.out.println( "free memory: " + rt.freeMemory() );
         }
     }
+    @RequestMapping(method = RequestMethod.GET,value = "info")
+    public String   serverInfo() throws UnknownHostException {
+        InetAddress addr = InetAddress.getLocalHost();
+        String ip=addr.getHostAddress().toString(); //获取本机ip
+        String hostName=addr.getHostName().toString(); //获取本机计算机名称
+        System.out.println(ip);
+        System.out.println(hostName);
+        String result = "hostname is %s"+"<br/>"+"ip is %s <br/>";
+        return  String.format(result,hostName,ip);
+    }
+
 }
